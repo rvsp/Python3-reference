0e5ee587c34ba38d75cd961aabd5e27ee16dfc25
tactfulness <pvenkat.rv@gmail.com>
2020-01-31 10:02:29 +0530

    conditional

---
+++ 4_conditional-statements.py
@@ -0,0 +1,45 @@
+'''
+Owner: Venkatasubramanian
+Topic: Operators
+'''
+
+'''
+if statements
+if...else statements
+elif statements
+'''
+
+var1 = 100
+# if statements
+if var1:
+   print("Got a true expression value")
+   print(var1)
+
+
+# if...else statements
+if var1:
+   print("Got a true expression value")
+   print(var1)
+else:
+   print("Got a false expression value")
+   print(var1)
+
+# elif statements
+var = 100
+if var == 200:
+   print("1 - Got a true expression value")
+   print(var)
+elif var == 150:
+   print("2 - Got a true expression value")
+   print(var)
+elif var == 100:
+   print("3 - Got a true expression value")
+   print(var)
+else:
+   print("4 - Got a false expression value")
+   print(var)
+
+print("Good bye!")
+
+
+# Your task is to understand and implement the nested if statement

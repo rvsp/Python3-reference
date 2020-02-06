ff4aae54f7d9767b9b838e1eb6ccb923c2c95dff
tactfulness <pvenkat.rv@gmail.com>
2020-02-06 10:17:58 +0530

    modified

--- 7_lists.py
+++ 7_lists.py
@@ -11,10 +11,13 @@ count()	   -  Returns the number of elements with the specified value
 extend()   -  Add the elements of a list (or any iterable), to the end of the current list
 index()	   -  Returns the index of the first element with the specified value
 insert()   -  Adds an element at the specified position
-pop()	   -  Removes the element at the specified position
+pop()	     -  Removes the element at the specified position
 remove()   -  Removes the first item with the specified value
 reverse()  -  Reverses the order of the list
 sort()	   -  Sorts the list 
+min()      -  Returns minimum value from list
+max()      -  Returns maximum value from list
+len()      -  Return length
 '''
 
 cars = ['Ford', 'Mitsubishi', 'BMW', 'VW']
@@ -60,3 +63,7 @@ print(cars)
 temp.clear()
 print(temp)
 
+ll = [23,45,67,98,4,76,33,43]
+print(min(ll))
+
+print(max(ll))
